8774c5a956b8064522b7025ec8fc66d3695a6f35
Vladislav Melnikov <81232295+mindoken@users.noreply.github.com>
2023-03-15 18:51:56 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from utils import start,caps,echo,find_photo,download_video
+from utils import start,create_form,echo
 from telegram import *
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler    
 
@@ -6,12 +6,12 @@ def Run():
     application = ApplicationBuilder().token('6207411726:AAGTkgz8niKVarLEqwj4mbpE7MfXEFJLC1Q').build()
 
     start_handler = CommandHandler('start', start)
-    caps_handler = CommandHandler('caps', caps)
+    create_form_handler = CommandHandler('create_form', create_form)
     echo_handler = CommandHandler('echo',echo)
     find_photo_handler = CommandHandler('find_photo',find_photo)
     download_video_handler = CommandHandler('download_video',download_video)
     application.add_handler(start_handler)
-    application.add_handler(echo_handler)
+    application.add_handler(create_form_handler)
     application.add_handler(caps_handler)
     application.add_handler(find_photo_handler)
     application.add_handler(download_video_handler)
