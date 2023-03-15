ee64c7fdda5286782c44616a20a87add3d19d41e
Vladislav Melnikov <81232295+mindoken@users.noreply.github.com>
2023-03-15 18:26:46 +0300

    Add files via upload

---
+++ form.py
@@ -0,0 +1,13 @@
+class Form:
+    tg_id=""
+    gender=""
+    name=""
+    age=""
+    photo=""
+    tg_chat_id=""
+    facility=""
+    networking=False
+    friend=False
+    partnership=False
+    help=False
+    chatting=False
