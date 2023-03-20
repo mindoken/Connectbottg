ae237a27c7957483c0cfb88c41416b4b795dab25
Vladislav Melnikov <81232295+mindoken@users.noreply.github.com>
2023-03-21 01:27:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import logging
 from run import Run
 
 
-logging.basicConfig(
+logging.basicConfig( #базовые настройки конфигурации
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
 if __name__ == '__main__':
-    Run()
+    Run() #запуск метода, который активирует систему
